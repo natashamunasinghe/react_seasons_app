76de69d6c6c40ab1e00783a249bcf30dcd09cc7a
Natasha <natashakhema@gmail.com>
2019-01-02 19:57:53 +1100

    add helper function

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ class App extends React.Component {
     }
 
 
-    render () {
+    //helper function (instead of having conditional login inside render)
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -34,7 +35,15 @@ class App extends React.Component {
         }
  
         return<Spinner message="Please accept location request" />;
+    }
 
+
+    render () {
+        return (
+            <div className= "border red">
+                {this.renderContent()}
+            </div>
+        );  
     }
 }
 
